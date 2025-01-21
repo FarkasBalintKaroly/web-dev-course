183643ce098ba0f292661f279a9942e8c5c67a63
BalintFarkas <balint.karoly13@gmail.com>
2025-01-21 15:27:42 +0100

    Complete QR code generator project in js

--- 04_backend/qr-code-generator/index.js
+++ 04_backend/qr-code-generator/index.js
@@ -1,14 +1,18 @@
 // Using the inquirer npm package to get user input.
-import inquirer from 'inquirer'
+import inquirer from 'inquirer';
+import qr from 'qr-image';
+import fs from 'fs';
 
 const question = [
     {
-        type: 'input',
-        name: 'to-qr',
-        message: 'What to generate to QR code?'
+        name: 'URL',
+        message: 'Type in your URL: ?'
     },
-]
+];
 
 inquirer.prompt(question).then((answers => {
-    console.log(answers);
-}))
+    const url = answers.URL;
+    console.log(url);
+    var qr_png = qr.image(url, {type: 'png'});
+    qr_png.pipe(fs.createWriteStream('url-qr.png'));
+}));
